d858cdb3839b4699788ec3afcb50ca50161e0a87
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-04 00:57:51 +0300

    Ürünler Tablomuza Karşılık Gelen Sınıf
    Veri tabanımızdaki Tabloya Karşılık Gelen Sınıfımız

---
+++ Product.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockAmount { get; set; }
+
+    }
+}
