c463ee0650dc3a7f463c298810c6d014fd251b36
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-04 00:54:33 +0300

    ETradeContext Class
    Veri Tabanı işlemlerinde kullanmak için oluşturduğumuz Context Class

---
+++ ETradeContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public class ETradeContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+
+
+}
