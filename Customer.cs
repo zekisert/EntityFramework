d665a48f9babaa3bbadc595274923887e30a5e76
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-06 23:07:29 +0300

    Create Customer.cs

---
+++ Customer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEfCodeFirst.Entities
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
