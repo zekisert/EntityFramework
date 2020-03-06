4edfe4066e2be766bc7fd9343edfb3cdb713e300
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-06 23:07:58 +0300

    Create Order.cs

---
+++ Order.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEfCodeFirst.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Customer Customer { get; set; }
+
+
+
+    }
+}
