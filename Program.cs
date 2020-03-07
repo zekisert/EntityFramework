d9104b8999e3d6f575ea8c62365f833bc225068f
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-07 15:58:04 +0300

    Program.cs Update
    Arakadaşlar Program.cs kodlarımız daha sade bir görünüme gelerek rahat çalışabilmeniz adına güncellenmiştir.

--- Program.cs
+++ Program.cs
@@ -1,97 +1,97 @@
-//using NorthwindEfCodeFirst.Contexts;
-//using NorthwindEfCodeFirst.Entities;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using EntityFrameworkMiddleLevel.Contexts;
+using EntityFrameworkMiddleLevel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace NorthwindEfCodeFirst
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            //NewMethod();
-            //Add();
-            //NewMethod1();
+namespace NorthwindEfCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriEkle();
+            MusteriListesi();
+            using (var northwindContext = new NorthwindContext())
+            {
+                foreach (var customer in northwindContext.Customers)
+                {
+                    Console.WriteLine("Customer Name : {0}", customer.ContactName);
+                }
+            }
+            Console.ReadLine();
+        }
 
-            // Silme Operasyonu
-            //using (var northwindContext = new NorthwindContext())
-            //{
-            //    Order order = northwindContext.Orders.Find(1);
-            //    northwindContext.Orders.Remove(order);
-            //    northwindContext.SaveChanges();
-            //}
+        private static void Sil()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Order order = northwindContext.Orders.Find(1);
+                northwindContext.Orders.Remove(order);
+                northwindContext.SaveChanges();
+            }
+        }
 
-            // Update İşlemi
-            //using (var northwindContext = new NorthwindContext())
-            //{
-            //    Customer customer = northwindContext.Customers.Find("ENGIN");
-            //    customer.Country = "Turkey";
-            //    northwindContext.SaveChanges();
-            //}
+        private static void Guncelle()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Customer customer = northwindContext.Customers.Find("ENGIN");
+                customer.Country = "Turkey";
+                northwindContext.SaveChanges();
+            }
+        }
 
-            //using (var northwindContext = new NorthwindContext())
-            //{
-            //    IQueryable<Customer> result = from c in northwindContext.Customers
-            //}
+        private static void SiparisEkle()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Customer customer = northwindContext.Customers.Find("ZEKİ");
+                customer.Orders.Add(new Order
+                {
+                    OrderID = 1,
+                    OrderDate = DateTime.Now,
+                    ShipCity = "Ankara",
+                    ShipCountry = "Türkiye"
+                });
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void MusteriEkle()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                var customer = new Customer
+                {
+                    CustomerID = "ZEKİ",
+                    City = "Bursa",
+                    CompanyName = "DijitalDonusum.com",
+                    ContactName = "Zeki SERT",
+                    Country = "Türkiye"
+                };
+
+                northwindContext.Customers.Add(customer);
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void MusteriListesi()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                foreach (var customer in northwindContext.Customers)
+                {
+                    Console.WriteLine("Customer Name : {0}", customer.ContactName);
+                }
+            }
+        }
+    }
+}
 
 
 
 
 
-
-        //    Console.ReadLine();
-        //}
-
-        // Sipariş Tablosundaki Ekleme İşlemi
-        //private static void NewMethod1()
-        //{
-        //    using (var northwindContext = new NorthwindContext())
-        //    {
-        //        Customer customer = northwindContext.Customers.Find("ENGIN");
-        //        customer.Orders.Add(new Order
-        //        {
-        //            OrderID = 1,
-        //            OrderDate = DateTime.Now,
-        //            ShipCity = "Ankara",
-        //            ShipCountry = "Türkiye"
-        //        });
-        //        northwindContext.SaveChanges();
-        //    }
-        //}
-
-
-        // Müşteri Eklemek
-        //private static void Add()
-        //{
-        //    using (var northwindContext = new NorthwindContext())
-        //    {
-        //        var customer = new Customer
-        //        {
-        //            CustomerID = "ENGIN",
-        //            City = "Ankara",
-        //            CompanyName = "YazilimDevi.Com",
-        //            ContactName = "Engin Demiroğ",
-        //            Country = "Türkiye"
-        //        };
-
-        //        northwindContext.Customers.Add(customer);
-        //        northwindContext.SaveChanges();
-        //    }
-        //}
-
-        // Müşteri Listesi Yazmak
-        //private static void NewMethod()
-        //{
-        //    using (var northwindContext = new NorthwindContext())
-        //    {
-        //        foreach (var customer in northwindContext.Customers)
-        //        {
-        //            Console.WriteLine("Customer Name : {0}", customer.ContactName);
-        //        }
-        //    }
-        //}
-//    }
-//}
